56e8e588d9c3986f715312ceff25c2fcf347e7b2
josiah <greensiah05@gmail.com>
2024-08-09 17:45:24 -0400

    Making the dropdown menu.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,33 @@
 const dropdown = document.querySelector(".dropdown-content");
 
+const dropdownBtn = document.querySelector(".dropBtn");
+
 function menuClick() {
   dropdown.classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropBtn')) {
-    let i;
-    for (i = 0; i > dropdown.length; i++) {
-      let openDropdown = dropdown[i];
-      if (openDropdown.classList.contains('show')) {
-        (openDropdown.classList.remove('show'));
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropBtn')) {
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show")
       }
     }
   }
-}
+
+// document.addEventListener('click', e => {
+//   if (dropdown.contains(e.target)) {
+//     menu.classList.remove("show");
+//   }
+// })
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropBtn')) {
+//     let i;
+//     for (i = 0; i > dropdown.length; i++) {
+//       let openDropdown = dropdown[i];
+//       if (openDropdown.classList.contains('show')) {
+//         (openDropdown.classList.remove('show'));
+//       }
+//     }
+//   }
+// }
 
